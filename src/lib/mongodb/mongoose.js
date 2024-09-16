61ebfc2b959f51a0a15512a71e941f1a2a1712fc
efobifrancis <francisefobi@gmail.com>
2024-09-16 19:09:29 +0100

    fixed db name in mongoose.js

--- src/lib/mongodb/mongoose.js
+++ src/lib/mongodb/mongoose.js
@@ -12,7 +12,7 @@ export const connect = async() => {
 
     try {
         await mongoose.connect(process.env.MONGODB_URI, {
-            dbName: 'next-auth-clerk-app',
+            dbName: 'next-auth-clerk',
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
